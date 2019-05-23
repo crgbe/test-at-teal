76a51c1a9e04d38eed6508a4aaa943a05a52e0e7
Credo Junior <username@example.com>
2019-05-23 21:14:07 +0000

    Writing the code to perform POST request within ajax

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -18,24 +18,34 @@ class WeatherController extends AbstractController
     public function index()
     {
         $client = new Client();
-//        $query = '{
-//            forecast(city: "casablanca"){
-//                day
-//                date
-//                low
-//                high
-//                text
-//                code
-//            }
-//        }';
-        $response = $client->request('GET', 'http://localhost:4000/graphql?query={forecast(city:"casablanca"){day date low high text code}}');
+        $response = $client->request('GET', 'http://localhost:4000/graphql?query={forecast(city:"chicago"){day date low high text code}}');
 
         $forecast = $response->getBody()->getContents();
         $forecast = json_decode($forecast);
 
 
-        return $this->render('weather.html.twig', [
+        return $this->render('index.html.twig', [
             'forecast' => $forecast,
+            'cities' => $this->cities,
+        ]);
+    }
+
+
+    /**
+     * @Route("/weather/{city}", name="weather_submit_city", methods={"POST"})
+     */
+    public function submitCity($city)
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'http://localhost:4000/graphql?query={forecast(city:\" '.$city.'\"){day date low high text code}}');
+
+        $forecast = $response->getBody()->getContents();
+        $forecast = json_decode($forecast);
+
+
+        return $this->render('index.html.twig', [
+            'forecast' => $forecast,
+            'cities' => $this->cities,
         ]);
     }
 }
