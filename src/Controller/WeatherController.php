689f3102bca0514f4f26385ba1126c7a0cb08e7d
Credo Junior <username@example.com>
2019-05-23 19:51:09 +0000

    Integration of MDB and fectching 1 day weather forecast for graphql

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -3,8 +3,39 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class WeatherController
+class WeatherController extends AbstractController
 {
+    private $cities = ['casablanca', 'sunnyvale', 'chicago', 'philadelphia', 'rabat'];
 
+    /**
+     * @Route("/weather")
+     */
+    public function index()
+    {
+        $client = new Client();
+//        $query = '{
+//            forecast(city: "casablanca"){
+//                day
+//                date
+//                low
+//                high
+//                text
+//                code
+//            }
+//        }';
+        $response = $client->request('GET', 'http://localhost:4000/graphql?query={forecast(city:"casablanca"){day date low high text code}}');
+
+        $forecast = $response->getBody()->getContents();
+        $forecast = json_decode($forecast);
+
+
+        return $this->render('weather.html.twig', [
+            'forecast' => $forecast,
+        ]);
+    }
 }
--- src/Entity/Forecast.php
+++ src/Entity/Forecast.php
@@ -4,7 +4,48 @@
 namespace App\Entity;
 
 
-class Forecast
+class Product
 {
+    private $day;
+
+    private $date;
+
+    private $low;
+
+    private $high;
+
+    private $text;
+
+    private $code;
+
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getLow()
+    {
+        return $this->low;
+    }
+
+    public function getHigh()
+    {
+        return $this->high;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
 
 }
